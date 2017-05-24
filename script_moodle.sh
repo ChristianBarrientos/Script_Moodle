7e6dd36e0600161e49f85ab51ccae3fce3dc781c
Christian Barrientos <christianbarrientoss@hotmail.com>
2017-05-24 14:34:52 -0300

    modifi

--- script_moodle.sh
+++ script_moodle.sh
@@ -29,7 +29,7 @@ exit
 ###Fin de promt mysql>
 
 
-apt-get install php7.0 php7.0-gd php7.0-mysql php7.0-cURL php7.0-xmlrpc php7.0-intl php7.0-json php7.0-cli php7.0-xml git -y --force-yes
+apt-get install php7.0 php7.0-gd php7.0-mysql php7.0-cURL php7.0-xmlrpc php7.0-intl php7.0-json php7.0-cli php7.0-xml php7.0-zip git -y --force-yes
 ##apt-get install php7.0-gd php7.0-mysql 
 ##apt-get install php7.0-cURL php7.0-xmlrpc php7.0-intl
 ##apt-get install php7.0-json
@@ -54,12 +54,12 @@ git clone https://github.com/moodle/moodle.git
 mkdir /var/moodledata
 chown www-data /var/moodledata/
 mysql -u dbuser -p
-cd www/html/
+cd /var/www/html/
 chown -R www-data /var/www/html/moodle
 
 ##Solo en la instalacion se debe ejecutar:
 chmod -R 777 /var/www/*
-chown -R www-data: var/www/
+chown -R www-data: /var/www/
 ##Despues se debe cambiar los permisos.
 
 
@@ -69,9 +69,48 @@ chown -R www-data: var/www/
 
 
 ##Ejecutar script
-php /var/www/hyml/moodle/admin/cli/install.php
+php /var/www/html/moodle/moodle/admin/cli/install.php
+###Choose a languaje
+##es   (de español)
+###Permiso directorio de datos
+## Por defecto 2777
+###Direccion Web
+##http://192.168.44.120/moodle/moodle (directorio donde se encuentra los arhcivos de moodle)
+###Directorio de Datos
+##Por Defecto(/var/www/moodledata)
+###Notice
+## y
+### Choosel database driver
+## Por defecto (mysqli)
+###Database host
+## Por defecto (Localhost. Poner la direccion si es distinto)
 #despues de ejecutarlo este generara un archivo llamado 'config.php' se le debe asignar propietario a www-data (apache) apra que se lo pueda ejecutar desde el cliente.
-
+###Database name
+## dbname (valor de dbname)
+###Tables prefix
+##Por defecto (md1_)
+###Database port
+##--mysql -u dbuser -p
+##--SHOW VARIABLES WHERE Variable_name IN ('hostname','port'); para saber puerto
+## 3306
+###Database user
+## dbuser
+###Database password
+##dbpass
+###Full site name (Nombre Largo)
+##http://192.168.44.120/moodle/moodle
+###Short name for site
+##localhost/moodle
+###Admin account username(usaurio administrador)
+##por defecto (admin. se puede cambiar)
+###New admin user password
+##pass del usuario administrador
+###New admin user email address
+##Por defecto (vacio)
+###Upgrade key (leave empty to not set it)
+##Por defecto (Vacia)
+###Copyright notice
+##y
 ##HASTA ACA FUNCIONANDO
 
 #Agregando para que envie mails
